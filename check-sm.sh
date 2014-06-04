0e13f2f4c94111a85d639752f4d2d8761f2afda4
Mate Lakat <mate.lakat@citrix.com>
2014-06-04 09:45:23 +0100

    add check-sm.sh to check an SM repo

---
+++ check-sm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eux
+
+PATH_TO_SM="$1"
+WORKDIR="$2"
+
+
+THISFILE=$(readlink -f $0)
+THISDIR=$(dirname $THISFILE)
+
+PATH_TO_SM=$(readlink -f $PATH_TO_SM)
+WORKDIR=$(readlink -f $WORKDIR)
+
+
+. "$THISDIR/sm_ci_lib.sh"
+
+[ -d "$WORKDIR" ]
+
+rm -f "$WORKDIR/sm.tgz"
+sm_pack_create "$PATH_TO_SM" "$WORKDIR/sm.tgz"
+
+[ -d "$WORKDIR/smroot" ] || {
+    mkdir "$WORKDIR/smroot"
+    smroot_create "$WORKDIR/smroot"
+    smroot_install_prereqs "$WORKDIR/sm.tgz" "$WORKDIR/smroot"
+}
+
+
+smroot_prepare_venv "$WORKDIR/sm.tgz" "$WORKDIR/smroot"
+smroot_run_tests "$WORKDIR/smroot"
