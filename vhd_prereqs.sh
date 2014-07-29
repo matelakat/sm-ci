26f3f24584c4705b7d6cde9214fe72ba3be57614
Mate Lakat <mate.lakat@citrix.com>
2014-07-29 18:03:19 +0100

    Added doxygen to generate a report
    To have some call graphs.

--- vhd_prereqs.sh
+++ vhd_prereqs.sh
@@ -1,6 +1,8 @@
 set -eux
 apt-get -qy install autoconf libtool libaio-dev uuid-dev make libxen-dev \
-    python-pip
+    python-pip graphviz
+
+apt-get -qy --no-install-recommends install doxygen
 
 pip install gcovr
 
