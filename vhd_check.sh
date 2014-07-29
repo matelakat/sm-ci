bebc9dfe6ba9cf625bfc34ec44a667ba7317cb2e
Mate Lakat <mate.lakat@citrix.com>
2014-07-29 16:01:24 +0100

    Add compiler flags to instrument the code
    This way we'll get coverage information

--- vhd_check.sh
+++ vhd_check.sh
@@ -4,6 +4,8 @@ mkdir /blktap
 cd blktap
 tar -xzf /source.tgz
 ./autogen.sh
+export CFLAGS="-fprofile-arcs -ftest-coverage -g"
+export CPPLAGS="-fprofile-arcs -ftest-coverage -g"
 ./configure
 cd vhd
 make
