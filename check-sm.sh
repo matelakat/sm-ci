8e99c25d8a4bf2c53863023ae4854ed297efd564
Mate Lakat <mate.lakat@citrix.com>
2014-08-08 17:54:54 +0100

    Refactor: move sm related functions to check-sm.sh

--- check-sm.sh
+++ check-sm.sh
@@ -59,6 +59,93 @@ THISDIR=$(dirname $THISFILE)
 
 . "$THISDIR/ci_lib.sh"
 
+
+function install_sm_prereqs() {
+    local sm_tarball
+    local chroot_dir
+    sm_tarball="$1"
+    chroot_dir="$2"
+
+    chroot_dir="$(readlink -f $chroot_dir)"
+
+    local fakechroot_state
+    local chroot_path
+
+    fakechroot_state="$chroot_dir/$FAKECHROOT_FNAME"
+    chroot_path="$chroot_dir/$CHROOT_SUBDIR"
+
+    [ -e "$sm_tarball" ]
+    [ -e "$fakechroot_state" ]
+    [ -d "$chroot_path" ]
+
+    cp "$sm_tarball" "$chroot_path/source.tgz"
+
+    fakeroot -i "$fakechroot_state" -s "$fakechroot_state" fakechroot chroot \
+        "$chroot_path" bash -c \
+            "rm -rf /storage-manager \
+            && mkdir -p /storage-manager/sm \
+            && cd /storage-manager/sm \
+            && tar -xzf /source.tgz \
+            && bash tests/install_prerequisites_for_python_unittests.sh"
+}
+
+
+function prepare_sm_venv() {
+    local sm_tarball
+    local chroot_dir
+    sm_tarball="$1"
+    chroot_dir="$2"
+
+    chroot_dir="$(readlink -f $chroot_dir)"
+
+    local fakechroot_state
+    local chroot_path
+    fakechroot_state="$chroot_dir/$FAKECHROOT_FNAME"
+    chroot_path="$chroot_dir/$CHROOT_SUBDIR"
+
+    [ -e "$sm_tarball" ]
+    [ -e "$fakechroot_state" ]
+    [ -d "$chroot_path" ]
+
+    cp "$sm_tarball" "$chroot_path/source.tgz"
+
+    fakeroot -i "$fakechroot_state" -s "$fakechroot_state" fakechroot chroot \
+        "$chroot_path" bash -c \
+            "rm -rf /storage-manager \
+            && mkdir -p /storage-manager/sm \
+            && cd /storage-manager/sm \
+            && tar -xzf /source.tgz \
+            && bash tests/setup_env_for_python_unittests.sh"
+}
+
+
+function run_sm_tests() {
+    local sm_tarball
+    local chroot_dir
+    sm_tarball="$1"
+    chroot_dir="$2"
+
+    chroot_dir="$(readlink -f $chroot_dir)"
+
+    local fakechroot_state
+    local chroot_path
+    fakechroot_state="$chroot_dir/$FAKECHROOT_FNAME"
+    chroot_path="$chroot_dir/$CHROOT_SUBDIR"
+
+    [ -e "$sm_tarball" ]
+    [ -e "$fakechroot_state" ]
+    [ -d "$chroot_path" ]
+
+    cp "$sm_tarball" "$chroot_path/source.tgz"
+
+    fakeroot -i "$fakechroot_state" -s "$fakechroot_state" fakechroot chroot \
+        "$chroot_path" bash -c \
+            "cd /storage-manager/sm \
+            && rm -rf drivers tests \
+            && tar -xzf /source.tgz \
+            && bash tests/run_python_unittests.sh"
+}
+
 check "$PATH_TO_SM" "$WORKDIR" \
-    "chroot_install_sm_prereqs chroot_prepare_sm_venv" \
-    "chroot_run_sm_tests"
+    "install_sm_prereqs prepare_sm_venv" \
+    "run_sm_tests"
