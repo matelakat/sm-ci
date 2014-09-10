68988789c2e1961f63a22535a289d7d7c909dbd4
Mate Lakat <mate.lakat@citrix.com>
2014-09-10 14:36:06 +0100

    Install ceedling
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>

--- blktap_prereqs.sh
+++ blktap_prereqs.sh
@@ -1,8 +1,11 @@
 set -eux
 apt-get -qy install autoconf libtool libaio-dev uuid-dev make libxen-dev \
-    python-pip graphviz libssl-dev
+    python-pip graphviz libssl-dev rubygems
 
 apt-get -qy --no-install-recommends install doxygen
 
 pip install gcovr
 pip install nose
+
+cd /
+HOME=/root gem install ceedling
