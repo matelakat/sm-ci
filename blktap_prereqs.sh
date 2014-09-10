4408344f8305211f3e3df2123c71caba02a2e0fa
Mate Lakat <mate.lakat@citrix.com>
2014-09-10 10:32:58 +0100

    Add libssl-dev dependency
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>

--- blktap_prereqs.sh
+++ blktap_prereqs.sh
@@ -1,6 +1,6 @@
 set -eux
 apt-get -qy install autoconf libtool libaio-dev uuid-dev make libxen-dev \
-    python-pip graphviz
+    python-pip graphviz libssl-dev
 
 apt-get -qy --no-install-recommends install doxygen
 
