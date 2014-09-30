9fef8eb009bcc2e4ee2ceec12ae2ac365a2cecff
Flavien Quesnel <flavien.quesnel@citrix.com>
2014-09-30 15:55:49 +0100

    First draft to run blktap unit tests on docker
    See README.docker to actually run the unit tests for blktap on docker
    
    Signed-off-by: Flavien Quesnel <flavien.quesnel@citrix.com>

--- blktap_prereqs.sh
+++ blktap_prereqs.sh
@@ -1,4 +1,6 @@
 set -eux
+
+# rubygems does not exist for trusty anymore; install ruby instead
 apt-get -qy install autoconf libtool libaio-dev uuid-dev make libxen-dev \
     python-pip graphviz libssl-dev rubygems
 
